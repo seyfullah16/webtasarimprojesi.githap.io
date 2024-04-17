73c69b05ebbdc418e3f2f1360ee00be5e08f3fa2
seyfullah16 <167296473+seyfullah16@users.noreply.github.com>
2024-04-17 11:42:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#accordion").accordion();
+})
+// $(function(){
+
+// })
+$(function(){
+    $("#tabs").tabs();
+})
+
+$(function(){
+    $("#resizable").resizable();
+ })
